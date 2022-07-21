6604753b0512fd3cd86e41fa884b825ee0cd6f04
tamal15 <tamal15-13243@diu.edu.bd>
2022-07-20 22:17:00 -0700

    back

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ require('dotenv').config();
 const cors = require("cors");
 const app=express();
 const port = process.env.PORT || 5000;
-
+app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
-app.use(express.json())
+// app.use(express.json())
 
 
 
@@ -29,7 +30,7 @@ async function run() {
         console.log("connected to database");
         const database = client.db('education');
         const userCollection = database.collection('users');
-        
+        const manyquestionCollection = database.collection('manyQuextion');
 
 
 
@@ -53,7 +54,53 @@ async function run() {
             const updateDoc= {$set : user}
             const result= userCollection.updateOne(filter,updateDoc,option)
             res.json(result)
-        })
+        });
+
+
+        app.post('/postQuestion', async(req,res) =>{
+            const user=req.body;
+            console.log(user)
+            const result=await manyquestionCollection.insertOne(user);
+            res.json(result)
+        });
+
+        
+
+        app.get("/allQuestions", async (req, res) => {
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            const query = req.query;
+            delete query.page
+            delete query.size
+            Object.keys(query).forEach(key => {
+                if (!query[key])
+                    delete query[key]
+            });
+
+            if (Object.keys(query).length) {
+                const cursor = manyquestionCollection.find(query, status = "approved");
+                const count = await cursor.count()
+                const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+                // console.log(allQuestions)
+                res.json({
+                    allQuestions, count
+                });
+               
+            } else {
+                const cursor = manyquestionCollection.find({
+                    
+                });
+                // console.log(cursor)
+                const count = await cursor.count()
+                const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+                // console.log(allQuestions)
+                res.json({
+                    allQuestions, count
+                });
+            }
+
+        });
+
 
         
 
